d85d4b5e52824fd99c8dd84f5bc8749c3030e8d8
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2023-10-11 16:55:03 +0700

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,132 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import MagicInputReal from './MagicInputReal/MagicInputReal';
 
 function App() {
-  const [inputStatus, setInputStatus] = useState('default');
+  const [emailStatus, setEmailStatus] = useState('default');
+  const [emailValue, setEmailValue] = useState('');
+  const [emailError, setEmailError] = useState('');
 
-  const [inputValue, setInputValue] = useState('');
-  const [inputType, setInputType] = useState('password');
-  let hint = 'Just type in!';
-  let errorMessage = 'password fake';
-  
-  /*
-  validate the input value here
-  */
+  const [email2Status, setEmail2Status] = useState('default');
+  const [email2Value, setEmail2Value] = useState('');
+  const [email2Error, setEmail2Error] = useState('');
+
+  const [passwordStatus, setPasswordStatus] = useState('default');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const [numberStatus, setNumberStatus] = useState('default');
+  const [numberValue, setNumberValue] = useState('');
+  const [numberError, setNumberError] = useState('');
+
+  useEffect(() => {
+    if (emailValue) {
+      console.log('abc');
+      const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (emailValue.toLocaleLowerCase().match(emailRegex)) {
+        setEmailStatus('success');
+        setEmailError('');
+      } else {
+        setEmailStatus('error');
+        setEmailError(<span style={{ color: 'red' }}>Email is not valid</span>)
+      }
+    }
+  }, [emailValue])
+
+  useEffect(() => {
+    if (email2Value) {
+      console.log('abc');
+      const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (email2Value.toLocaleLowerCase().match(emailRegex)) {
+        setEmail2Status('success');
+        setEmail2Error('');
+      } else {
+        setEmail2Status('error');
+        setEmail2Error(<span style={{ color: 'red' }}>Email is not valid</span>)
+      }
+    }
+  }, [email2Value])
 
   return (
     <div className='inputs'>
+      This is a fake form:
       <MagicInputReal
-        inputStatus='success'
-        inputValue={inputValue}
-        inputType={inputType}
+        inputStatus={emailStatus}
+        inputValue={emailValue}
+        inputType="text"
 
-        label="Hello world"
+        label="Email"
+        placeholder="Enter your email"
         isRequired={true}
-        hint={hint}
-        errorMessage={errorMessage}
-        icon='lock'
+        hint={'Email should have this format: \'abc@xyz.ijk\''}
+        errorMessage={emailError}
+        icon='heart'
         focusTheme='#5570F1'
-        
-        setInputValue={setInputValue}
-        setInputType={setInputType}
-        setInputStatus={setInputStatus} />
+
+        setInputValue={setEmailValue}
+        setInputStatus={setEmailStatus} />
+
+      <MagicInputReal
+        inputStatus={email2Status}
+        inputValue={email2Value}
+        inputType="text"
+
+        placeholder="Enter your email"
+        isRequired={true}
+        errorMessage={email2Error}
+        icon='heart'
+        focusTheme='#5570F1'
+
+        setInputValue={setEmail2Value}
+        setInputStatus={setEmail2Status} />
+
+      <MagicInputReal
+        inputStatus={passwordStatus}
+        inputValue={passwordValue}
+        inputType="password"
+
+        label="Password"
+        placeholder="Enter your password"
+        isRequired={true}
+        hint={'Email should have this format: \'abc@xyz.ijk\''}
+        errorMessage={passwordError}
+        icon='lock'
+        focusTheme='#FF20FF'
+
+        setInputValue={setPasswordValue}
+        setInputStatus={setPasswordStatus} />
+
+      <MagicInputReal
+        inputStatus='disabled'
+        inputValue=''
+        inputType="password"
+
+        label="Password-2"
+        placeholder="Enter your password"
+        isRequired={false}
+        hint={'Email should have this format: \'abc@xyz.ijk\''}
+        errorMessage={passwordError}
+        icon='lock'
+        focusTheme='#FF20FF'
+
+        setInputValue={setPasswordValue}
+        setInputStatus={setPasswordStatus} />
+
+      <MagicInputReal
+        inputStatus={numberStatus}
+        inputValue={numberValue}
+        inputType="number"
+
+        label="Number"
+        placeholder="0"
+        isRequired={true}
+        hint={'Email should have this format: \'abc@xyz.ijk\''}
+        errorMessage={numberError}
+        icon='lock'
+        focusTheme='red'
+
+        setInputValue={setNumberValue}
+        setInputStatus={setNumberStatus} />
     </div>
   )
 }
